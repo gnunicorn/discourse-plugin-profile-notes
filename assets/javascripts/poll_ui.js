451f8a337f3977e6f645ba7533398c9a4db3e507
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-02 02:18:40 +0530

    Don't need an IIFE, Discourse does that already.

--- assets/javascripts/poll_ui.js
+++ assets/javascripts/poll_ui.js
@@ -1,86 +1,81 @@
-(function() {
+var PollView = Ember.View.extend({
+  templateName: "poll",
+  classNames: ['poll-ui'],
+  options: [],
+  showResults: false,
 
-  var PollView = Ember.View.extend({
-    templateName: "poll",
-    classNames: ['poll-ui'],
-    options: [],
-    showResults: false,
+  updateOptionsFromJSON: function(json) {
+    if (json["selected"]) { this.set('showResults', true); }
 
-    updateOptionsFromJSON: function(json) {
-      if (json["selected"]) { this.set('showResults', true); }
+    array = [];
+    Object.keys(json["options"]).forEach(function(option) {
+      array.push(Ember.Object.create({
+        option: option,
+        votes: json["options"][option],
+        checked: (option == json["selected"])
+      }));
+    });
+    this.set('options', array);
+  },
 
-      array = [];
-      Object.keys(json["options"]).forEach(function(option) {
-        array.push(Ember.Object.create({
-          option: option,
-          votes: json["options"][option],
-          checked: (option == json["selected"])
-        }));
-      });
-      this.set('options', array);
-    },
+  replaceElement: function(target) {
+    this._insertElementLater(function() {
+      target.replaceWith(this.$());
+    });
+  },
 
-    replaceElement: function(target) {
-      this._insertElementLater(function() {
-        target.replaceWith(this.$());
-      });
-    },
-
-    actions: {
-      selectOption: function(option) {
-        if (!this.get('controller.currentUser.id')) {
-          this.get('controller').send('showLogin');
-          return;
-        }
-
-        this.get('options').forEach(function(opt) {
-          opt.set('checked', opt.get('option') == option);
-        });
-        this.rerender();
-
-        this.set('loading', true);
-        Discourse.ajax("/poll", {
-          type: "PUT",
-          data: {post_id: this.get('post.id'), option: option}
-        }).then(function(newJSON) {
-          this.set('showResults', true);
-          this.set('loading', false);
-          this.updateOptionsFromJSON(newJSON);
-          this.rerender();
-        }.bind(this));
-      },
-
-      toggleShowResults: function() {
-        this.set('showResults', !this.get('showResults'));
-      }
-    }
-  });
-
-  Discourse.PostView.reopen({
-    createPollUI: function($post) {
-      var post = this.get('post');
-
-      if (!post.get('topic.title').match(/^poll:/i) || post.get('post_number') > 1) {
+  actions: {
+    selectOption: function(option) {
+      if (!this.get('controller.currentUser.id')) {
+        this.get('controller').send('showLogin');
         return;
       }
 
-      var poll_details = post.get('poll_details');
-      var view = this.createChildView(PollView, {
-        controller: this.get('controller'),
-        post: post
+      this.get('options').forEach(function(opt) {
+        opt.set('checked', opt.get('option') == option);
       });
-      view.updateOptionsFromJSON(poll_details);
-      view.replaceElement($post.find("ul:first"));
-      this.set('pollView', view);
+      this.rerender();
 
-    }.on('postViewInserted'),
+      this.set('loading', true);
+      Discourse.ajax("/poll", {
+        type: "PUT",
+        data: {post_id: this.get('post.id'), option: option}
+      }).then(function(newJSON) {
+        this.set('showResults', true);
+        this.set('loading', false);
+        this.updateOptionsFromJSON(newJSON);
+        this.rerender();
+      }.bind(this));
+    },
 
-    clearPollView: function() {
-      if (this.get('pollView')) {
-        this.get('pollView').destroy();
-      }
-    }.on('willClearRender')
-  });
+    toggleShowResults: function() {
+      this.set('showResults', !this.get('showResults'));
+    }
+  }
+});
 
+Discourse.PostView.reopen({
+  createPollUI: function($post) {
+    var post = this.get('post');
 
-})();
+    if (!post.get('topic.title').match(/^poll:/i) || post.get('post_number') > 1) {
+      return;
+    }
+
+    var poll_details = post.get('poll_details');
+    var view = this.createChildView(PollView, {
+      controller: this.get('controller'),
+      post: post
+    });
+    view.updateOptionsFromJSON(poll_details);
+    view.replaceElement($post.find("ul:first"));
+    this.set('pollView', view);
+
+  }.on('postViewInserted'),
+
+  clearPollView: function() {
+    if (this.get('pollView')) {
+      this.get('pollView').destroy();
+    }
+  }.on('willClearRender')
+});
